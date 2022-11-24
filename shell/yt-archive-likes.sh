ff1c929022cffb9a9ac8440898bf218eebc1040c
Sebastien Belanger <seb.belanger@gmail.com>
2022-11-25 00:06:59 +0100

    Updated YTDL scripts with yt-dlp versions

--- shell/yt-archive-likes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-WORK_DIR='/mnt/storage/root/videos/youtube'
-youtube-dl --playlist-reverse --download-archive $WORK_DIR/liked_videos/downloaded.txt --cookies $WORK_DIR/youtube.txt -i --sleep-interval 5 --max-sleep-interval 30 -o "$WORK_DIR/liked_videos/%(playlist_index)s - %(title)s [%(id)s]/%(playlist_index)s - %(title)s [%(id)s].%(ext)s" -f bestvideo[ext=mp4]+bestaudio[ext=m4a] --merge-output-format mp4 --add-metadata --write-description --write-info-json --write-annotations --write-thumbnail https://www.youtube.com/playlist?list=LL
